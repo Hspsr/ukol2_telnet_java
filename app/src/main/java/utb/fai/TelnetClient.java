8c13488b8012ccc202af3b573a1aaaf34e2d1108
Hspsr <119581613+Hspsr@users.noreply.github.com>
2024-10-06 20:55:11 +0200

    Update TelnetClient.java

--- app/src/main/java/utb/fai/TelnetClient.java
+++ app/src/main/java/utb/fai/TelnetClient.java
@@ -7,6 +7,7 @@ public class TelnetClient {
 
     private String serverIp;
     private int port;
+    private String consoleInput;
 
     public TelnetClient(String serverIp, int port) {
         this.serverIp = serverIp;
@@ -16,11 +17,61 @@ public class TelnetClient {
     public void run() {
         try {
             Socket socket = new Socket(serverIp, port);
-            // Implementation of receiving and sending data
-            // Implement processing of input from the user and sending data to the server
-            // Implement response processing from the server and output to the console
+
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader outputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter serverWriter = new PrintWriter(socket.getOutputStream(), true);
+            
+                        
+            Thread inputThread = new Thread(){
+                @Override
+                public void run() {   
+                    try{
+                        while ((consoleInput = inputReader.readLine()) != null) {
+                            serverWriter.println(consoleInput);
+                            if (consoleInput.equalsIgnoreCase("/QUIT")) {
+                                socket.close();
+                                break;
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }                     
+                }
+            };
+
+            Thread outputThread = new Thread(){
+                @Override
+                public void run(){
+                    try{
+                        String serverResponse;
+                        while((serverResponse = outputReader.readLine()) != null){
+                            System.out.println(serverResponse);
+                            if (serverResponse.equalsIgnoreCase("/QUIT")){
+                                break;
+                            }
+                        } 
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            inputThread.start();
+            outputThread.start();
+
+            inputThread.join();
+            outputThread.join();
+
+            outputReader.close();
+            serverWriter.close();
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        } finally {
+            System.exit(0);
         }
     }
 }
